0056f6422321b519418d3b7c5ed572e38c23f0c6
Sunny Wan <SunnyWan2020@gmail.com>
2025-01-26 16:59:24 -0500

    Got RAG working

--- ScholiumModel/model.py
+++ ScholiumModel/model.py
@@ -1,67 +1,117 @@
 import os
-from langchain_cohere import ChatCohere
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, MessagesState, StateGraph
-from langchain_core.messages import HumanMessage, SystemMessage, trim_messages, AIMessage
 
-#API Key
+from langchain_cohere import CohereEmbeddings, ChatCohere
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+
+from typing_extensions import List, TypedDict
+
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import MessagesState, StateGraph
+
+from langgraph.prebuilt import create_react_agent
+
+
+#Another security risk
+os.environ["LANGSMITH_API_KEY"] = "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"
 os.environ["COHERE_API_KEY"] = "pwYNrztZvPYTflPTWTQuLjbF27ES4kr6OMoCt3wf"
+os.environ["PINECONE_API_KEY"] = "pcsk_3wj9Yc_Z4qKuzouzoT3gRmkjMZEQE3pdiXYqfM3krLaMkbdvkiwHcqdQYYbfbVLML2XUH"
 
-#Model
-class CohereModel:
-    def __init__(self):  
-        self.model = ChatCohere(model="command-r-plus")
-        self.chat_history = StateGraph(state_schema=MessagesState)
-        self.chat_history.add_edge(START, "model")
-        self.chat_history.add_node("model", self.call_model)
-        self.memory = MemorySaver()
-        self.app = self.chat_history.compile(checkpointer=self.memory)
-        self.trimmer = trim_messages(
-                max_tokens=65,
-                strategy="last",
-                token_counter= self.model,
-                include_system=True,
-                allow_partial=False,
-                start_on="human",
-            )
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+model = ChatCohere(model="command-r-plus")
+
+embeddings = CohereEmbeddings(model="embed-english-v3.0")
+
+pc = Pinecone(api_key= pinecone_api_key)
+index = pc.Index("scholium-index")
+vector_store = PineconeVectorStore(embedding=embeddings, index=index)
+
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized_docs = []
+    for doc in retrieved_docs:
+        formatted_doc = f"Source: {doc.metadata}\nContent: {doc.page_content}"
+        serialized_docs.append(formatted_doc)
+
+    serialized = "\n\n".join(serialized_docs)
+    return serialized, retrieved_docs
+
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = model.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    return {"messages": [response]}
+
+tools = ToolNode([retrieve])
+
+def generate(state: MessagesState):
+    '''
+    Generates the desired answer to the user's question
+    '''
+    recent_tool_messages = []
+    for message in state["messages"]:
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages
+
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise. Also make sure to cite the context where you got the answer from."
+        "\n\n"
+        f"{docs_content}"
+    )
+    prompt = [SystemMessage(system_message_content)]
+    for message in state["messages"]:
+        if message.type in ("human", "system") or (message.type == "ai" and not message.tool_calls):
+            prompt.append(message)
+
+    response = model.invoke(prompt)
+    return {"messages": [response]}
+
+def _compile_graph():
+    graph_builder = StateGraph(MessagesState)
+    graph_builder.add_node(query_or_respond)
+    graph_builder.add_node(tools)
+    graph_builder.add_node(generate)
+
+    graph_builder.set_entry_point("query_or_respond")
+    graph_builder.add_conditional_edges(
+        "query_or_respond",
+        tools_condition,
+        {END: END, "tools": "tools"},
+    )
+    graph_builder.add_edge("tools", "generate")
+    graph_builder.add_edge("generate", END)
+
+    return graph_builder.compile()
 
 
-    def call_model(self, state: MessagesState):
-        trimmed_messages = self.trimmer.invoke(state["messages"])
-        response = self.model.invoke(trimmed_messages)
-        return {"messages": response}
-
-
-def get_output(query, config, app):
-    input_messages = [HumanMessage(query)]
-    response = ""
-    for chunk, metadata in app.stream(
-        {"messages": input_messages},
-        config,
-        stream_mode="messages",
-    ):
-        if isinstance(chunk, AIMessage):  # Filter to just model responses
-            response += " " + chunk.content
-
-
-    return response
-
-
-
-def create_config(thread_id: str):
-    return {"configurable": {"thread_id": thread_id}}
+def get_RAG():
+    return _compile_graph()
 
 
 
 if __name__ == "__main__":
-    model = CohereModel()
-    config = create_config("test")
-    queries = ["Hi my name is Bob", "What's my name?"]
-    for query in queries:
-        print("Human Query: ", query)
-        print(get_output(query, config, model.app))
+    graph = get_RAG()
+    input_message = "Give me papers about transformers"
 
-    config2 = create_config("test2")
-    print("---New thread---")
-    print("Human Query: ", queries[-1])
-    print(get_output(queries[-1], config2, model.app))
+    for step in graph.stream(
+        {"messages": [{"role": "user", "content": input_message}]},
+        stream_mode="values",
+    ):
+        step["messages"][-1].pretty_print()
