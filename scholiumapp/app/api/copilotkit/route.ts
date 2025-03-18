c0f261edaa52515731263a4a91b4f057b8c5bfa4
Sunny Wan <SunnyWan2020@gmail.com>
2025-03-18 16:16:50 -0400

    Update route.ts

--- scholiumapp/app/api/copilotkit/route.ts
+++ scholiumapp/app/api/copilotkit/route.ts
@@ -29,7 +29,7 @@ import {
         },
       ],
     }) : copilotKitEndpoint({
-      url: process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",// http://localhost:8000/copilotkit for local devlopment https://scholium-api.vercel.app//copilotkit
+      url: process.env.REMOTE_ACTION_URL || "https://scholium-api.vercel.app//copilotkit",// http://localhost:8000/copilotkit for local devlopment https://scholium-api.vercel.app//copilotkit
     })
     
     const runtime = new CopilotRuntime({
