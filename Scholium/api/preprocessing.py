19a1efe1e846cf4ebab7e95da1ae26a53bdf95d2
Sunny Wan <SunnyWan2020@gmail.com>
2025-03-22 22:31:13 -0400

    Update preprocessing.py

--- Scholium/api/preprocessing.py
+++ Scholium/api/preprocessing.py
@@ -1,4 +1,4 @@
-from api.openalex import WorksHandler
+from api.openalex import WorksHandler, IDHandler
 from api.text_splitter import extract_search_parameters
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -7,20 +7,31 @@ load_dotenv()
 
 client = OpenAI()
 
-def search_parameters_to_search(query, client):
+
+def search_parameters_to_search(query: str, client: OpenAI, idhandler: IDHandler, workshandler = WorksHandler):
     params = extract_search_parameters(query, client=client)
     filters= {}
     if params.authors:
-        pass
+        authors = [idhandler.get_author_id(author) for author in params.authors]
+        filters["author"] = ",".join(authors)
+        
     if params.concepts:
-        pass
+        concepts = [idhandler.get_topic_id(concept) for concept in params.concepts]
+        filters["concept"] = ",".join(concepts)
+        
     if params.institutions:
-        pass
+        institutions = [idhandler.get_institution_id(institution) for institution in params.institutions]
+        filters["institution"] = ",".join(institutions)
 
-    if params.language: filters["language"] = params.language   
+    if params.language: filters["language"] = params.language  
+
     n_papers = params.n_papers if params.n_papers else 10
-    print(params)
+    
+    results = workshandler.search(query=query,filters=filters, n_results= n_papers)
+    print(results)
 
 if __name__ == '__main__':
+    idhandler = IDHandler("sunny@scholium.ai")
+    wh = WorksHandler("sunny@scholium.ai")
     query = "Find 15 papers about transformer models by Vaswani and Hinton in English from U of T"
-    search_parameters_to_search(client=client, query=query)
+    search_parameters_to_search(client=client, query=query, idhandler=idhandler, workshandler=wh)
