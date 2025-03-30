21e798f2006fe544027030409a67ca01fad91cde
Sunny Wan <SunnyWan2020@gmail.com>
2025-03-30 19:39:28 -0400

    added check boxes and cards for each response

--- scholiumapp/lib/agent-state.tsx
+++ scholiumapp/lib/agent-state.tsx
@@ -4,21 +4,28 @@ export type ResearchState = {
     //   answer: string;
     //   metadata: string;
     answer: {
-        markdown: string;
-        paper_metadata: Array<{
+        results: Array<{
           title: string;
-          authors: Array<Array<string>>;
-          publication_date: string;
-          doi: string;
-          journal: string;
-          biblio?: {
-            volume?: string;
-            issue?: string;
-            first_page?: string;
-            last_page?: string;
-          };
-          referenced_works?: Array<string>;
-          related_works?: Array<string>;
+          summary: string;
+          id: string;
+          metadata: Array<{
+            title: string;
+            authors: Array<{
+                      first_name: string;
+                      last_name: string;
+                    }>;
+            publication_date: string;
+            doi: string;
+            journal: string;
+            biblio?: {
+              volume?: string;
+              issue?: string;
+              first_page?: string;
+              last_page?: string;
+            };
+            referenced_works?: Array<string>;
+            related_works?: Array<string>;
+          }>;
         }>;
       };
   }
